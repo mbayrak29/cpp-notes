a1a3047c7a45cf2e04e9c1c835a130a116973b97
Metehan Bayrak <metehannbayrak@gmail.com>
2025-12-23 17:07:59 +0300

    variables.cpp

---
+++ 01-basics/variables.cpp
@@ -0,0 +1,27 @@
+// Topic: Variables and Output
+// Description:
+// This program demonstrates how to print text to the console
+// and how to use variables to create dynamic output.
+// It shows the difference between fixed text and variable-based output.
+
+#include<iostream>
+using namespace std;
+
+int main() {
+// Printing fixed (hard-coded) text
+	cout << "Mete eats hambuger." << endl;
+	cout << "Mete drinks coke." << endl;
+	cout << "Mete is 22 years old." << endl;
+  
+ // Declaring variables
+	string name;
+	name = "Mustafa";
+	int age = 25;
+  
+ // Printing dynamic text using variables
+	cout << name << " eats hambuger." << endl;
+	cout << name << " drinks coke." << endl;
+	cout << name << " is " << age << " years old." << endl;
+
+  return 0;
+}
