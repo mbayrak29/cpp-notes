7186380f1c563fff1b4fa99a34652af9df39d5cf
Metehan Bayrak <metehannbayrak@gmail.com>
2025-12-23 16:43:24 +0300

    Create main.cpp

---
+++ 01-basics/main.cpp
@@ -0,0 +1,15 @@
+// Topic: Basic C++ Program Structure
+// Description:
+// This program prints a simple message to the console.
+// It demonstrates the basic structure of a C++ program,
+// including header files, main function, and output using cout.
+
+#include<iostream> 
+using namespace std; 
+
+int main() { 
+  
+	cout << "Hello C++" << endl;
+	return 0;
+  
+}
