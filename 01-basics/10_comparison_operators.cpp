dc2dbffdd623ad11e1301e637b9a83b3dcef65a0
Metehan Bayrak <metehannbayrak@gmail.com>
2025-12-26 16:41:20 +0300

    10_comparison_operators.cpp

---
+++ 01-basics/10_comparison_operators.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+
+    /*
+    Comparison Operators Examples
+
+    ==  equal to
+    !=  not equal to
+    >   greater than
+    <   less than
+    >=  greater than or equal to
+    <=  less than or equal to
+    */
+
+    int a = 10;
+    int b = 20;
+
+    cout << "a == b: " << (a == b) << endl;
+    cout << "a != b: " << (a != b) << endl;
+    cout << "a > b: " << (a > b) << endl;
+    cout << "a < b: " << (a < b) << endl;
+    cout << "a >= b: " << (a >= b) << endl;
+    cout << "a <= b: " << (a <= b) << endl;
+
+	return 0;
+}
