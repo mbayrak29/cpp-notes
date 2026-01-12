ad28e84cc784b351eebdb8e17f92b87b1218a3ed
Metehan Bayrak <metehannbayrak@gmail.com>
2026-01-12 20:40:16 +0300

    car.h

---
+++ 09_oop/01_car_class/car.h
@@ -0,0 +1,19 @@
+/*
+    File: Car.h
+    Description:
+    Defines the Car class and its properties.
+*/
+
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Car
+{
+public:
+    string colour;
+    string model;
+    int horsePower;
+
+    void showLisance();
+};
