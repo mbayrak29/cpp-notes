dcd9a4e524e6652252f6ac69cf94724b165a77e9
Metehan Bayrak <metehannbayrak@gmail.com>
2026-01-12 20:42:33 +0300

    01_Car.cpp

---
+++ 09_oop/01_Car.cpp
@@ -0,0 +1,14 @@
+/*
+    File: Car.cpp
+    Description:
+    Implements the showLisance() function of the Car class.
+*/
+
+#include "Car.h"
+
+void Car::showLisance()
+{
+    cout << "Model: " << model << endl;
+    cout << "Colour: " << colour << endl;
+    cout << "Horse Power: " << horsePower << endl;
+}
